db2df2858d46406ae10dcd5ae9615d6c58ea07db
ArtiomShashmurin <artiom.shashmurin@gmail.com>
2025-07-07 14:16:23 +0300

    Class and def

--- Camera_record_pause_name.py
+++ Camera_record_pause_name.py
@@ -1,51 +1,61 @@
 import cv2
 import os 
 
+class CameraRec:
 
-camera = cv2.VideoCapture(0)
+    def __init__ (self):
+        self.camera = cv2.VideoCapture(0)
+
+        if not self.camera.isOpened():
+            print ('Error opened')
+            return
+        self.firstName = "test.mp4"
+        self.recording = False
+        self.fps = 30.0
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
+        self.Run()
+
+        self.camera.release()
+        cv2.destroyAllWindows()
+
+    def Run(self):
+        while True:
+            ret, frame = self.camera.read()
+
+            if not ret:
+                print ('Error ret')
+                break
+
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == ord('q'):
+                break
+            elif key == ord(' '):
+                self.RecordingCamera(frame)
+
+            if self.recording:
+                self.RedCircle(frame)
+                self.record.write(frame)
 
 
-if not camera.isOpened():
-        print ("Erroropened")
+            cv2.imshow('Result', frame)
 
-
-recording = False
-key_q = ord ('q')
-key_space = ord(' ')
-second_name = 'test.mp4'
-
-while True:
-    ret , frame = camera.read()
-    key = cv2.waitKey(1)& 0xFF
-
-    if not ret:
-        print("Error ret")
-        break
-
-    if key == key_q:
-        break
-    elif key == key_space:
-
-        if not recording:
-            record = cv2.VideoWriter(second_name, cv2.VideoWriter_fourcc(*'mp4v'),30.0,(frame.shape[1],frame.shape[0]))
-            recording = True        
+    def RecordingCamera(self, frame):
+        
+        if not self.recording:
+            self.record = cv2.VideoWriter(self.firstName, self.fourcc,self.fps, (frame.shape[1], frame.shape[0]))
+            self.recording = True
         else:
-            recording = False
-            record.release()
-            new_name = input("New name record: ")
-            if not new_name:
-                 new_name = "Noname"
-            new_fileName = new_name + '.mp4'
+            self.recording = False
+            self.record.release()
+            self.newName = input("New Name: ")
+            if not self.newName:
+                self.newName = self.firstName
+            self.newNameFile = self.newName + '.mp4'
+            os.rename(self.firstName, self.newNameFile)
 
-            os.rename(second_name, new_fileName)
+    def RedCircle (self, frame):
+        cv2.circle(frame, (10,10),5,(0,0,255),thickness=-1)
 
-                 
-    if recording:
-         cv2.circle(frame, (10,10),5,(0,0,255),thickness=-1)
-         record.write(frame)
-    
-    cv2.imshow('Frame', frame)
-
-
-camera.release()
-cv2.destroyAllWindows()
+CameraRec()
